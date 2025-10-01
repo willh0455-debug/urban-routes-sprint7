4224e905f299803cfaedebcbecc807314de77d8c
willh0455-debug <willh0455@gmail.com>
2025-10-01 14:49:49 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-"""# This file stores test data constants for Project 7
+# This file stores test data constants for Project 7
 
 # URL of the Urban Routes app (for now, empty string until we start the server)
 URBAN_ROUTES_URL = ''
@@ -10,18 +10,3 @@ PHONE_NUMBER = '+1 123 123 12 12'
 CARD_NUMBER = '1234 5678 9100'
 CARD_CODE = '1111'
 MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
-data.py - Sprint 7 test data (exact names per spec)
-"""
-
-# Paste your live server URL between the quotes (keep the quotes; one line)
-URBAN_ROUTES_URL =URBAN_ROUTES_URL = 'https://cnt-bced91cb-d502-4ae9-811c-2937f61fe6c2.containerhub.tripleten-services.com/'
-# Required constants
-ADDRESS_FROM = 'East 2nd Street, 601'
-ADDRESS_TO   = '1300 1st St'
-PHONE_NUMBER = '+1 123 123 12 12'
-CARD_NUMBER  = '1234 5678 9100'
-CARD_CODE    = '1111'
-MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
-
-# Optional helper
-TIMEOUT_SECONDS = 5
