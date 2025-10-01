7a8b301c116f816f5d4e8255420f3837050d394c
willh0455-debug <willh0455@gmail.com>
2025-10-01 14:48:49 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,15 @@
-"""
+"""# This file stores test data constants for Project 7
+
+# URL of the Urban Routes app (for now, empty string until we start the server)
+URBAN_ROUTES_URL = ''
+
+# Test data constants
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
 data.py - Sprint 7 test data (exact names per spec)
 """
 
