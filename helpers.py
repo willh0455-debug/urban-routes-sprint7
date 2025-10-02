7aad752777cb2bbc39cab314cd6e84fd4efc1934
willh0455-debug <willh0455@gmail.com>
2025-10-01 20:13:05 -0500

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -43,7 +43,7 @@ def is_url_reachable(url):
         with urllib.request.urlopen(url, context=ssl_ctx) as response:
             # print("Response Status Code:", response.status) #for debugging purposes
             if response.status == 200:
-                 return True
+     return True
             else:
                 return False
     except Exception as e:
